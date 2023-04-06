0069526b4962bdc5eaccc91f0e5d8a2e3a9d56bb
tbetancur <tbetancur@unal.edu.co>
2023-04-05 20:43:44 -0500

    Lab5
    Actualizado

--- pregunta.py
+++ pregunta.py
@@ -12,22 +12,37 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    def clean_data():
-
-        df = pd.read_csv("solicitudes_credito.csv", sep=";")
-        df.rename(columns={'Unnamed: 0':'index'},inplace=True)
-        df.set_index('index', inplace=True)
-
-        df.sexo = df.sexo.str.lower().astype(str).str.strip()
-        df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.capitalize().str.strip()
-        df.idea_negocio = df.idea_negocio.str.replace('_',' ', regex=False).str.replace('_',' ',regex=False).str.capitalize().str.strip
-        df.barrio = df.barrio.str.replace('_','-').str.replace('-',' ').str.lower()
-        df.estrato = df.estrato.astype("str").str.capitalize()
-        df.comuna_ciudadano = df.comuna_ciudadano.astype(str).str.capitalize()
-        df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"], dayfirst= True)
-        df.monto_del_credito = df.monto_del_credito.str.replace(",","",regex=True).str.replace("$","",regex=True).str.strip
-        df.monto_del_credito = df.monto_del_credito.astype(float)
-        df.linea_credito=df.linea_credito.str.replace('-',' ', regex=False).str.replace('_',' ',regex=False).str.capitalize()
+    
+    df.drop('Unnamed: 0', axis=1, inplace=True)
+    df.dropna(how='any', inplace=True)
+    df.drop_duplicates(inplace=True)
+    
+    df['sexo'] = df['sexo'].apply(lambda x: x.lower().strip())
+    
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(lambda x: x.lower().strip())
+    
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().strip())
+    df['idea_negocio'] = df['idea_negocio'].str.replace('-', ' ')
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_', ' ')
+    df['idea_negocio'] = df['idea_negocio'].str.strip()
+    
+    df['barrio'] = df['barrio'].astype(str)
+    df['barrio'] = df['barrio'].apply(str.lower)
+    df.barrio = df.barrio.str.replace("_", " ", regex=False)
+    df.barrio = df.barrio.str.replace("-", " ", regex=False)
+    df.barrio = df.barrio.str.replace(".", " ", regex=False)
+    df.barrio = df['barrio'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
+    
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: int(x.split('$ ')[-1].split('.')[0].replace(',','')))
+    
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().strip())
+    df['línea_credito'] = df['línea_credito'].str.replace('-', ' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_', ' ')
+    df['línea_credito'] = df['línea_credito'].str.strip()
+  
+    df.dropna(how='any', inplace=True)
+    df.drop_duplicates(inplace=True)
   
     return df
